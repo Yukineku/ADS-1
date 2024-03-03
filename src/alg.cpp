0bd8580ffe5bc9c7e0045baaeb22910dfa9827ca
Yukineku <115874122+Yukineku@users.noreply.github.com>
2024-03-03 21:03:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,42 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if(value <= 1) return false;
+  if(value == 2) return true;
+    for(uint64_t i = 2; i * i <= value; i++){
+      if(value % i == 0){
+          return false;
+      }
+    }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int count = 0;
+  int number = 2;
+  while (true) {
+    if (checkPrime(number)) {
+      count++;
+      if (count == n) return number;
+    }
+    number++;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t next = value + 1;
+  while(!checkPrime(next)){
+    next++;
+  }
+  return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for(uint64_t i = 2; i < hbound; i++){
+    if (checkPrime(i)){
+      sum+=i;
+    }
+  }
+  return sum;
 }
